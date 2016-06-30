bdeb09cebe00cebcee35f8eddec9788df6d94e29
langecrew <langecrew@gmail.com>
2016-06-30 18:49:42 -0400

    Created CoinAcceptor object to do coin identification

---
+++ VendingMachineKata/test/vendingmachine/CoinAcceptorTest.java
@@ -0,0 +1,12 @@
+package vendingmachine;
+
+import org.junit.Test;
+
+public class CoinAcceptorTest {
+
+	@Test
+	public void testCoinAcceptorCorrectlyIdentifiesCoinsBySizeAndWeight() {
+		CoinAcceptor coinAcceptor = new CoinAcceptor();
+	}
+
+}
