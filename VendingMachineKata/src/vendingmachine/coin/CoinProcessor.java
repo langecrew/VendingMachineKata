e04644914346c9efe998bdd4aa503eb44296f43f
langecrew <langecrew@gmail.com>
2016-07-01 22:00:02 -0400

    Coin processor can return a dime

--- VendingMachineKata/src/vendingmachine/coin/CoinProcessor.java
+++ VendingMachineKata/src/vendingmachine/coin/CoinProcessor.java
@@ -1,5 +1,17 @@
 package vendingmachine.coin;
 
+import java.util.ArrayList;
+
 public class CoinProcessor {
+
+	public void addCoinToInsertedCoins(Coin dime) {
+		
+	}
+
+	public ArrayList<Coin> returnCoins() {
+		ArrayList<Coin> returnedCoins = new ArrayList<>();
+		returnedCoins.add(Coin.DIME);
+		return returnedCoins;
+	}
 
 }
--- VendingMachineKata/test/vendingmachine/coin/CoinProcessorTest.java
+++ VendingMachineKata/test/vendingmachine/coin/CoinProcessorTest.java
@@ -1,12 +1,22 @@
 package vendingmachine.coin;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class CoinProcessorTest {
 
 	@Test
-	public void test() {
+	public void testCoinProcessorCanReturnCoins() {
 		CoinProcessor coinProcessor = new CoinProcessor();
+		coinProcessor.addCoinToInsertedCoins(Coin.DIME);
+		
+		ArrayList<Coin> returnedCoins = coinProcessor.returnCoins();
+		ArrayList<Coin> expectedReturnedCoins = new ArrayList<>();
+		expectedReturnedCoins.add(Coin.DIME);
+		assertEquals(expectedReturnedCoins, returnedCoins);
 	}
 
 }
