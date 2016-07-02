79973d8c7333d233e39e9b3fa9c22a634ae88b51
langecrew <langecrew@gmail.com>
2016-07-01 20:37:06 -0400

    Adding a coin processor class
    To remove coin handling from main vending machine class

---
+++ VendingMachineKata/src/vendingmachine/coin/CoinProcessor.java
@@ -0,0 +1,5 @@
+package vendingmachine.coin;
+
+public class CoinProcessor {
+
+}
---
+++ VendingMachineKata/test/vendingmachine/coin/CoinProcessorTest.java
@@ -0,0 +1,12 @@
+package vendingmachine.coin;
+
+import org.junit.Test;
+
+public class CoinProcessorTest {
+
+	@Test
+	public void test() {
+		CoinProcessor coinProcessor = new CoinProcessor();
+	}
+
+}
